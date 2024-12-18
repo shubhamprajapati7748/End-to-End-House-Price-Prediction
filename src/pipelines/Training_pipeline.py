4a7e9fc2f04e0bbd14277678c63f22d8e0c0386b
shubhamprajapati7748 <shubhamprajapati7748@gmail.com>
2024-12-18 19:48:43 +0530

    updated-files

--- src/pipelines/Training_pipeline.py
+++ src/pipelines/Training_pipeline.py
@@ -7,7 +7,7 @@ obj=DataIngestion()
 train_data_path,test_data_path=obj.initiate_data_ingestion()
 
 data_transformation=DataTransformation()
-train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+train_arr,test_arr, _= data_transformation.initialize_data_transformation(train_data_path,test_data_path)
 
 model_trainer_obj=ModelTrainer()
 model_trainer_obj.initate_model_training(train_arr,test_arr)
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,6 +3,7 @@ import sys
 import pickle
 from src.logger import logging
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
@@ -15,21 +16,25 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+      
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
-        for i in range(len(models)):
-            model = list(model.vlues())[i]
-            # train model 
-            model.fit(X_train, y_train)
-            # Predict testing data 
-            y_pred = model.predict(X_test) 
-            model_score = r2_score(y_test, y_pred) 
-            report[list(model.keys())[i]] = model_score
-        return report 
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
     except Exception as e:
-        logging.error("Exception occured during model training")
+        logging.error("Exception occured during model evaluation")
         raise CustomException(e, sys)
+    
     
 def load_object(file_path):
     try:

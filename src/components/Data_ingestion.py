505f95b3c1f15105b838e87f010cc154e8d91971
shubhamprajapati7748 <shubhamprajapati7748@gmail.com>
2024-12-18 19:17:32 +0530

    added-data-ingestion

--- src/components/Data_ingestion.py
+++ src/components/Data_ingestion.py
@@ -17,7 +17,30 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        pass 
+        logging.info("Data ingestion started")
+
+        try:
+            data = pd.read_csv("dataset/BostonHousing.csv")
+            logging.info("Data read successfully")
+
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)), exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path, index=False)
+            logging.info("Data is saved in raw_data.csv")
+
+            logging.info("Train-test data splitting")
+            train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+            logging.info("Data splitting is completed")
+
+            train_data.to_csv(self.ingestion_config.training_data_path, index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path, index=False)
+            logging.info("Created the train and test data csv file")
+            logging.info("Data ingestion completed.")
+
+            return (self.ingestion_config.training_data_path, self.ingestion_config.test_data_path)
+        
+        except Exception as e:
+            logging.error("Error occured in data ingestion")
+            raise CustomException(e, sys)
 
 if __name__ == "__main__":
     # object = DataIngestion()
